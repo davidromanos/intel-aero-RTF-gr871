7993e8691f214a30b4faea254df2328124b457ef
Thomas Jespersen <thomasj@tkjelectronics.dk>
2017-05-21 18:11:44 +0200

    Visualization of projected landmarks compared to the actual measurement

--- src/FastSLAM/InverseMeasurementModel.m
+++ src/FastSLAM/InverseMeasurementModel.m
@@ -1,16 +1,21 @@
-function WorldLandmark = InverseMeasurementModel(pose, z, intrin)
+function WorldLandmark = InverseMeasurementModel(pose, z, intrin, CamOffset)
     c_psi = cos(pose(6));
     s_psi = sin(pose(6));
     c_theta = cos(pose(5));
     s_theta = sin(pose(5));
     c_phi = cos(pose(4));
-    s_phi = sin(pose(4));
-
+    s_phi = sin(pose(4));    
+    
     % Rotation matrix corresponding to: BC_R' * EB_R'
     R = [(-c_psi*s_theta*s_phi + s_psi*c_phi), (-s_psi*s_theta*s_phi-c_psi*c_phi), -(c_theta*s_phi);
             (-c_psi*s_theta*c_phi-s_psi*s_phi),  (-s_psi*s_theta*c_phi+c_psi*s_phi), -(c_theta*c_phi);
             (c_psi*c_theta),                     (s_psi*c_theta),                    -(s_theta)];
 
+    % EB_R = R_yaw * R_pitch * R_roll;
+    EB_R =  [ (c_theta*c_psi), (c_psi*s_theta*s_phi - c_phi*s_psi), (s_phi*s_psi + c_phi*c_psi*s_theta);
+              (c_theta*s_psi), (c_phi*c_psi + s_theta*s_phi*s_psi), (c_phi*s_theta*s_psi - c_psi*s_phi);
+              (-s_theta),      (c_theta*s_phi),                     (c_theta*c_phi)];
+        
     c_zl = z(3);
     c_xl = (z(1)*c_zl - intrin.ppx*c_zl) / intrin.fx;
     c_yl = (z(2)*c_zl - intrin.ppy*c_zl) / intrin.fy;
@@ -21,4 +26,4 @@ function WorldLandmark = InverseMeasurementModel(pose, z, intrin)
 
     TempLandmark = CamLandmark + R*pose_xyz;
 
-    WorldLandmark = R' * TempLandmark; % rot.transpose() corresponds to EB_R * BC_R
+    WorldLandmark = R' * TempLandmark + EB_R*CamOffset; % rot.transpose() corresponds to EB_R * BC_R
--- src/FastSLAM/MeasurementVisualization.m
+++ src/FastSLAM/MeasurementVisualization.m
@@ -1,6 +1,11 @@
 format long g;
 run('LoadLatestLogs.m');
 
+CamOffset = [0.09,-0.032,0.005]';
+
+%CamOffset = [0.12,0.015,0.10]'; % this seem to be the best when checking here in MATLAB, but it does not give better results when running the FastSLAM system
+%CamOffset = [0.09,-0.032,0.05]';
+
 %% Plot Mocap data
 figure(1);
 %plot3(pos(:,1), pos(:,2), pos(:,3));
@@ -49,11 +54,23 @@ ylim([0.9 4]);
 zlim([0 2]);
 
 hold on;
-
 for (i = 1:100:length(tMoc))
     R = rpy2tr(roll(i), pitch(i), yaw(i));
     R(1:3,4) = pos(i,:)';
     trplot(R, 'rgb', 'notext', 'noarrow', 'thick', 1, 'length', 0.2);
+end
+hold off;
+
+%% Plot actual landmark positions
+hold on;
+for i = 1:length(markers)
+    text(markers(i,2),...
+         markers(i,3),...
+         markers(i,4),...
+         num2str(markers(i,1)));
+    scatter3(markers(i,2),...
+             markers(i,3),...
+             markers(i,4),'O');
 end
 hold off;
 
@@ -86,7 +103,7 @@ end
 %% Calculate landmark position in a given frame (inverse measurement model)
 % RGB intrinsics should be used
 clc;
-i = 1000; % pick camera measurement
+i = 560; % pick camera measurement
 t = tCamReduced(i);
 
 iCam = find(tCam == t);
@@ -103,12 +120,22 @@ pose = mocap(iMoc,2:7)';
         R(1:3,4) = pose(1:3);
         trplot(R, 'color', 'red', 'notext', 'noarrow', 'thick', 3, 'length', 1);
         hold off;
+  
+zHat = [];        
 
 for (measIdx_ = measIdx)
     DetectedID = camera(iCam(measIdx_),2)
-    z = camera(iCam(measIdx_),3:5);
-    marker = InverseMeasurementModel(pose, z, RGB);
-
+    z = camera(iCam(measIdx_),3:5)
+    marker = InverseMeasurementModel(pose, z, RGB,CamOffset);
+    
+    midx = find(markers(:,1) == DetectedID);
+    if (length(midx) > 0)    
+        l = markers(midx,2:4)';
+        zHat = [zHat; MeasurementModel(pose, l, RGB,CamOffset)'];
+    else
+        zHat = [zHat; [-1,-1,-1]];
+    end
+    
     err = markers(:,2:4) - marker';
     errd = err(:,1).^2 + err(:,2).^2 + err(:,3).^2;
     [e idx] = min(errd);
@@ -120,3 +147,13 @@ for (measIdx_ = measIdx)
         disp('ERROR: Detected and Calculated marker ID does NOT match');
     end
 end
+
+figure(4);
+scatter(camera(iCam,3), -camera(iCam,4), '*');
+hold on;
+scatter(zHat(:,1), -zHat(:,2), 'O');
+text(zHat(:,1), -zHat(:,2), num2str(camera(iCam,2)));
+hold off;
+xlim([0 320]);
+ylim([-240 0]);
+legend('Camera measurement', 'Projected using known location');
